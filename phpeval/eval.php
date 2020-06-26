6e8aa109e83227f98343b69dcc924d9ad25dbd55
Stefantutor <lebohec.stefan@orange.fr>
2020-06-26 09:12:40 +0200

    Add files via upload

---
+++ phpeval/eval.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eval php</title>
+</head>
+<body>
+
+<form name="formulaire" method="get" action="traitement.php">
+Nom du prospect<input type="text" name="slbnom"/><br>
+Ville<input type="text" name="slbville"/><br>
+Téléphone<input type="text" name="slbtel"/><br>
+Mail<input type="text" name="slbmail"/><br>
+<input type="submit" name="slbvalider" value="send"/>
+</form>
+    
+</body>
+</html>
